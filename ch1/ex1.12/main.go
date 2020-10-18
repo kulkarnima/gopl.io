dcdea61a3e07dcd8aaf19aa228c7edb46254e3d5
INTINFRA\kulkarnim <makarand.kulkarni@mavenir.com>
2020-10-18 18:04:23 +0530

    Type conversion to use strconv.Atoi

--- ch1/ex1.12/main.go
+++ ch1/ex1.12/main.go
@@ -20,7 +20,7 @@ const (
     blackIndex = 1 // next color in palette
 )
 
-func lissajous(out io.Writer, cycles float64) {
+func lissajous(out io.Writer, cycles int) {
     const (
      // cycles  = 5     // number of complex x oscillator revolutions
         res     = 0.001 // angular resolution
@@ -36,7 +36,7 @@ func lissajous(out io.Writer, cycles float64) {
     for i := 0; i < nframes; i++ {
         rect := image.Rect(0, 0, 2*size+1, 2*size+1)
         img := image.NewPaletted(rect, palette)
-        for t := 0.0; t < cycles*2*math.Pi; t += res {
+        for t := 0.0; t < float64(cycles)*2*math.Pi; t += res {
             x := math.Sin(t)
             y := math.Sin(t*freq + phase)
             img.SetColorIndex(size + int(x*size+0.5), size + int(y*size+0.5),
@@ -55,7 +55,7 @@ func main() {
             log.Print(err)
         }
 
-        cycles, err := strconv.ParseFloat(r.Form["cycles"][0], 64)
+        cycles, err := strconv.Atoi(r.Form["cycles"][0])
         if err != nil {
             log.Println("server4: can not parse cycle count!")
             os.Exit(1)
