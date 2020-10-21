afbee46706e988b17973aa8391d75866f1e781a7
INTINFRA\kulkarnim <makarand.kulkarni@mavenir.com>
2020-10-21 19:01:05 +0530

    temperature conversion package

---
+++ ch2/tempconv0/tempconv.go
@@ -0,0 +1,20 @@
+// Package tempconv performs celcius to farenheit temparature compuatations
+package tempconv
+
+import "fmt"
+
+type Celcius float64
+type Farenheit float64
+
+const (
+    AbsoluteZeroC   Celcius = -273.15
+    FreezingC       Celcius = 0
+    BoilingC        Celcius = 100
+)
+
+func CToF(c Celcius) Farenheit { return Farenheit(c*9/5 + 32) }
+func FToC(f Farenheit) Celcius { return Celcius((f-32) * 5 / 9) }
+
+//!-
+
+func (c Celcius) String() String {return fmt.Sprintf("%g deg C", c) }
